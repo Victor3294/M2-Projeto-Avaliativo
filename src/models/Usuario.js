b336a0d4ca96c4d635f119bf48b357dab935aafa
Victor3294 <vblum26@gmail.com>
2024-07-27 13:09:18 -0300

    fix: 🐛 Correção nos models de Usuario e Local de coleta
    A correção feita foi de substituir o Sequelize, que estava sendo usado
    para definir os tipos das colunas, para dataTypes que seria o mais
    correto a se fazer

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,38 +1,39 @@
+const { DataTypes } = require("sequelize");
 const connection = require("../database/connection");
 
 const Usuario = connection.define('usuarios', {
     nome: {
-        type: Sequelize.STRING(125)
+        type: DataTypes.STRING(125)
       },
       data_de_nascimento: {
-        type: Sequelize.DATE
+        type: DataTypes.DATE
       },
       cpf: {
-        type: Sequelize.STRING(14),
+        type: DataTypes.STRING(14),
       },
       sexo: {
-        type: Sequelize.ENUM('Masculino', 'Feminino', 'Outro')
+        type: DataTypes.ENUM('Masculino', 'Feminino', 'Outro')
       },
       cep: {
-        type: Sequelize.STRING(9)
+        type: DataTypes.STRING(9)
       },
       email: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING
       },
       password_hash: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING
       },
       logradouro: {
-        type: Sequelize.STRING(150)
+        type: DataTypes.STRING(150)
       },
       uf: {
-        type: Sequelize.STRING(2)
+        type: DataTypes.STRING(2)
       },
       bairro: {
-        type: Sequelize.STRING(100)
+        type: DataTypes.STRING(100)
       },
       localidade: {
-        type: Sequelize.STRING(100)
+        type: DataTypes.STRING(100)
       }
 })
 

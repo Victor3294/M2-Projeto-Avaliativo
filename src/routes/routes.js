5ded68a69c8ccb44018b70bb2bb3236216cd2e84
Victor3294 <vblum26@gmail.com>
2024-07-27 15:42:30 -0300

    feat: 🎸 Criação do usarios.routes e da rota usuarios no routes
    A rota de usuarios é /usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,10 @@ const { Router } = require("express");
 const routes = new Router()
 
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./doc.swagger.json')
+const swaggerDocument = require('./doc.swagger.json');
+const usuariosRoutes = require("./usuarios.routes");
 
 routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+routes.use('/usuarios', usuariosRoutes)
 
 module.exports = routes
---
+++ src/routes/usuarios.routes.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const UsuariosController = require("../controllers/UsuariosController");
+
+const usuariosRoutes = new Router()
+
+module.exports = usuariosRoutes

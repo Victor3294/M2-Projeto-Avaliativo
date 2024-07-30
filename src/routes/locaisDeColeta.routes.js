e8e78f3b34249844853c3156ccf239e8b723cc7f
Victor3294 <vblum26@gmail.com>
2024-07-30 20:22:52 -0300

    docs: ✏️ Iniciando a documentação Swagger da API
    Apenas informações das rotas principais descritas, e foi separado em
    duas tags, a documentação está incompleta ainda.

--- src/routes/locaisDeColeta.routes.js
+++ src/routes/locaisDeColeta.routes.js
@@ -3,11 +3,47 @@ const LocaisDeColetaController = require("../controllers/LocaisDeColetaControlle
 
 const locaisDeColetaRoutes = new Router()
 
-locaisDeColetaRoutes.post('/', LocaisDeColetaController.criarLocalDeColeta)
-locaisDeColetaRoutes.get('/', LocaisDeColetaController.listarLocaisDeColeta)
-locaisDeColetaRoutes.get('/:id', LocaisDeColetaController.listarUmLocalDeColeta)
-locaisDeColetaRoutes.delete('/:id', LocaisDeColetaController.deletarUmLocalColeta)
-locaisDeColetaRoutes.put('/:id', LocaisDeColetaController.atualizarUmLocalDeColeta)
-locaisDeColetaRoutes.get('/:id/maps', LocaisDeColetaController.gerarLinkMapsLocalDeColeta)
+locaisDeColetaRoutes.post('/', LocaisDeColetaController.criarLocalDeColeta
+    /*
+    #swagger.tags = ['Locais de Coleta']
+    #swagger.description = 'Endpoint para criar um local de coleta'
+    #swagger.parameters.parameters['CriarLocalDeColeta'] = {
+        in: 'body',
+        description: 'Dados do local de coleta',
+        required: 'true',
+        schema: {
+            $nome_do_local: "Local de coleta X",
+            $descricao: "Descrição de um local de coleta especifico",
+            $cep: "44444-444"
+        }
+    }
+    
+    */
+)
+locaisDeColetaRoutes.get('/', LocaisDeColetaController.listarLocaisDeColeta
+    /**
+     * #swagger.tags = ['Locais de Coleta']
+     */
+)
+locaisDeColetaRoutes.get('/:id', LocaisDeColetaController.listarUmLocalDeColeta
+    /**
+     * #swagger.tags = ['Locais de Coleta']
+     */
+)
+locaisDeColetaRoutes.delete('/:id', LocaisDeColetaController.deletarUmLocalColeta
+    /**
+     * #swagger.tags = ['Locais de Coleta']
+     */
+)
+locaisDeColetaRoutes.put('/:id', LocaisDeColetaController.atualizarUmLocalDeColeta
+    /**
+     * #swagger.tags = ['Locais de Coleta']
+     */
+)
+locaisDeColetaRoutes.get('/:id/maps', LocaisDeColetaController.gerarLinkMapsLocalDeColeta
+    /**
+     * #swagger.tags = ['Locais de Coleta']
+     */
+)
 
 module.exports = locaisDeColetaRoutes
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,7 +3,41 @@ const UsuariosController = require("../controllers/UsuariosController");
 
 const usuariosRoutes = new Router()
 
-usuariosRoutes.post('/', UsuariosController.criarUsuario)
-usuariosRoutes.post('/login', UsuariosController.fazerLogin)
+usuariosRoutes.post('/', UsuariosController.criarUsuario
+        /*
+     #swagger.tags = ['Usuarios']
+     #swagger.description = 'Endpoint para criar um usuario'
+     #swagger.parameters['CriarUsuario'] = {
+        in:'body',
+        description: 'Dados do Usuario',
+        required: 'true',
+        schema: {
+            $nome: "Usuario Teste",
+            $data_de_nascimento: "2000-02-20",
+            $cpf: "111-111-111.11",
+            $sexo: "Masculino / Feminino / Outro",
+            $cep: "44444-444",
+            $email: "email@email.com",
+            $password_hash: "senha"
+        }
+     }
+     */
+)
+usuariosRoutes.post('/login', UsuariosController.fazerLogin
+    /*
+     #swagger.tags = ['Usuarios']
+     #swagger.description = 'Endpoint para logar um usuario'
+     #swagger.parameters['logarUsuario'] = {
+        in:'body',
+        description: 'Dados do Usuario',
+        required: 'true',
+        schema: {
+            $email: "email@email.com",
+            $password_hash: "senha"
+        }
+     }
+ */
+)
+
 
 module.exports = usuariosRoutes
